b8bfc451ca343d4fdbb8217f516f76b3827a256a
Joshua Hamann <haman127@tang.morris.umn.edu>
2019-09-04 18:35:29 -0500

    gain the output

--- regex/regex.sh
+++ regex/regex.sh
@@ -2,3 +2,5 @@
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
 sed -E 's/\* ([KNV]). ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
+
+sed -E 's/\* ([KNV])/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
